23c1cc8d70229cab52819e9a97ed5b4a11044c16
mohammed <mohammedsuhail95145@gmail.com>
2024-05-24 17:36:42 +0530

    Netflix Responsive and functionality

---
+++ src/app.js
@@ -0,0 +1,9 @@
+
+function signIn(){
+    let container = document.getElementById('container');
+    container.classList.add('hidden');
+
+    let signIn = document.getElementById('signIn');
+    signIn.classList.remove('hidden');
+    signIn.classList.add('block');
+}
